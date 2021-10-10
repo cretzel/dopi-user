2b124c0fa4ff8aaa7f3d45552ebad261e1f72a44
Nick Wiedenbrück <nick.wiedenbrueck@miele.com>
2021-10-10 23:49:42 +0200

    fix dev jwt

--- dopi-user-web/src/mock/devServer.js
+++ dopi-user-web/src/mock/devServer.js
@@ -9,7 +9,7 @@ const users = [
 module.exports = app => {
   app.use(bodyParser.json())
   app.route('/api/user/refresh').post((req, res) => {
-    res.cookie('dopi_jwt',"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6ImFkbWluIiwiUm9sZXMiOlsiYWRtaW4iXSwiZXhwIjoxNjMzODg4ODY4LCJpYXQiOjE2MzM4ODc5NjgsImlzcyI6ImRvcGkifQ.d0GFM8MOiFtZwlrHEC4EzCnmfqJC4_it6AkcZ67e6d4");
+    res.cookie('dopi_jwt',"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6ImFkbWluIiwiUm9sZXMiOlsiYWRtaW4iXSwiZXhwIjoxNjMzOTAzNDA2LCJpYXQiOjE2MzM5MDI1MDYsImlzcyI6ImRvcGkifQ.1K-v9jOp1UsxNuFP32nEblhD-gCQwkMiof74cMLxoPU");
     res.send(JSON.stringify({username: "admin"}))
   });
 
